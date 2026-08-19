399adae46e51053f3aaf6d6d412c9c1d82fdafc4
Om_Shelar <121863382+OmvShelar@users.noreply.github.com>
2026-08-19 07:58:44 +0530

    feat(ui): wrap app with FavoritesProvider at root

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
+import FavoritesProvider from './components/ui/FavoritesProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <FavoritesProvider>
+          <App />
+        </FavoritesProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
